010948c57a2e12011c0023a8c6ce6474d24e519b
jpark116 <jamespark116@gmail.com>
2024-04-08 11:49:28 -0400

    Update HeaderFooter.js

--- Final Project/HeaderFooter.js
+++ Final Project/HeaderFooter.js
@@ -3,37 +3,40 @@ const header = document.getElementById("header");
 header.innerHTML = `  
 <header> 
 <div class="pageheader">
-<a href="home.html" class="active">Placeholder for img</a>
+<a href="home.html" class="active"><img src=Logo.png class="Logo"></a>
 
 <!--Header links for website navigation -->
 <div id="linkheader">
-    <a href="Product.html">Product</a>
-    <a href="Contact.html">Contact</a>
-    <a href="About.html">About</a>
+    <a href="Product.html" class="link">Product</a>
+    <a href="Contact.html" class="link">Contact</a>
+    <a href="About.html" class="link">About</a>
 </div>
 
 <!--"Hamburger function to toggle links"-->
-<a href="javascript:void(0);" class="icon" onclick="myFunction()">
-<i><div></div><div></div><div></div></a>
-<script src="Hamburger.js"></script>
+<a class="icon" onclick="myFunction()">
+<i><div></div><div></div><div></div></i></a>
+</header>
 
-</div>
-<header>
+
+</menu>
     `
     function myFunction()
     {
         var x = document.getElementById("linkheader");
-        if (x.style.display === "block") {
+        if (x.style.display === "flex") {
             x.style.display = "none";
+            
         }
         else {
-            x.style.display = "block";
+            x.style.display = "flex";
         }
     }
 
     const footer = document.getElementById("footer")
 
     footer.innerHTML=`<footer>
-    <p class=footernote> Nintendo rights reserved 1989. </p> 
+    <p class=footernote> Nintendo rights reserved &#169;1989. </p> 
+    <p class=footernote> follow us </p>
+
     </footer>
     `
