3067408897d60b853e6dda6e4328749a5f7e3563
jpark116 <jamespark116@gmail.com>
2024-04-11 11:49:38 -0400

    Update HeaderFooter.js

--- Final-Project/HeaderFooter.js
+++ Final-Project/HeaderFooter.js
@@ -5,7 +5,7 @@ const header = document.getElementById("header");
 header.innerHTML = `  
 <header> 
 <div class="pageheader">
-<a href="index.html" class="active"><img src=Logo.png class="Logo"></a>
+<a href="https://jpark116.github.io/COMP1002-Final-Project/Final-Project/index.html" class="active"><img src=Logo.png class="Logo"></a>
 
 <!--Header links for website navigation -->
 <div id="linkheader">
